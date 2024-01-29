867c2860461e865e022eee39cf7e9233bb6cc05c
elkhoe <ekhoe.bg@gmail.com>
2024-01-29 23:50:09 +0700

    create nodejs server for nba data

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# qloo-node-project
+# Node.js Server for NBA Data
+
+## Overview
+
+This Node.js server is designed fetch data from an external API called Free NBA and organize each team into its respective division.
+
+## Endpoints
+
+- **GET /fetch-data**
+  - Fetches a list of NBA teams.
+  - Example: `GET /teams?page=0`
+
+- **GET /process-data**
+  - Processes the fetched teams data and organizes it by division.
+  - Returns a JSON object where each key is a division.
+  - Example: `GET /process-data`
--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-const app = require('express')();
-const PORT = 8080;
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios'); 
+const app = express();
+const PORT = process.env.PORT || 8080;
+const API_KEY = process.env.API_KEY;
+
+let data = []; 
+
+const options = {
+  method: 'GET',
+  url: 'https://free-nba.p.rapidapi.com/teams',
+  params: {page: '0'},
+  headers: {
+    'X-RapidAPI-Key': API_KEY,
+    'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+  }
+};
+
+// fetch-data endpoint
+app.get('/fetch-data', async (req, res) => {
+    try {
+        const response = await axios.request(options);
+        data = response.data.data;
+        res.json(data);
+    } catch (error) {
+        res.status(500).send('Error fetching data');
+    }
+});
+
+// process-data endpoint
+app.get('/process-data', (req, res) => {
+    try {
+        // Organize teams by division
+        const teamsByDivision = data.reduce((acc, team) => {
+            acc[team.division] = acc[team.division] || [];
+            acc[team.division].push(team);
+            return acc;
+        }, {});
+        
+        res.json(teamsByDivision);
+    } catch (error) {
+        res.status(500).send('Error processing data');
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
